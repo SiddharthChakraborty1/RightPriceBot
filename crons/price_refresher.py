935dc79c77137fff27d83bbac00259214a5beabf
Siddharth Chakraborty <sc9921777@gmail.com>
2025-02-20 22:00:59 +0530

    Minor: Added a script that fetches all users and checks current prices of all products, if any price is lower than or equal to the product's expected price, the user is notified of a change in price

--- crons/price_refresher.py
+++ crons/price_refresher.py
@@ -1,8 +1,54 @@
 from database.db_connector import initialize_db
 from managers.product_manager import ProductManager
+import os
+from dotenv import load_dotenv
+import telebot
+
 
 db = initialize_db()
+message_queue = {}
 
-product_handler = ProductManager(database=db)
-all_products = product_handler.fetch_all_products()
-print("all_products", all_products)
+def notify_users():
+    if not message_queue:
+        return
+    
+    TOKEN = os.environ.get("TOKEN")
+    bot = telebot.TeleBot(TOKEN)
+    for user_id in message_queue.keys():
+        bot.send_message(user_id, "Price of an item has changed")
+    
+    
+    
+
+def check_product_prices_for_user(product_handler: ProductManager, user_id: str, products: dict):
+    if not all([user_id, products]):
+        return
+    
+    notifiable_products = {}
+    
+    for product_id, product_info in products.items():
+        current_price = product_handler.fetch_product_price(product=product_info)
+        if current_price <= product_info.get("expected_price", 0):
+            product_info.update({"current_price": current_price})
+            notifiable_products[product_id] = product_info
+    
+    if not notifiable_products:
+        return
+    
+    message_queue[user_id] = notifiable_products
+
+
+def refresh_prices():
+    product_handler = ProductManager(database=db)
+    user_wise_products = product_handler.fetch_user_wise_products()
+    if not user_wise_products:
+        return
+    
+    for user_id, products in user_wise_products.items():
+        check_product_prices_for_user(product_handler, user_id, products)
+
+    message_queue and notify_users()
+        
+
+
+refresh_prices()
--- managers/product_manager.py
+++ managers/product_manager.py
@@ -1,11 +1,13 @@
 from firebase_admin import db
+from helpers.content_parsers import AmazonParser
 
 class ProductManager:
-    
+
     def __init__(self, database: db):
         self.db = database
-    
-    def fetch_all_products(self)->list:
+        self.parser: AmazonParser = AmazonParser()
+
+    def fetch_user_wise_products(self)->list:
         """
         This is an unsafe method as it fetches all users at once and iterates over them
         to fetch all of their products that have been saved, only use if dataset is small or retrieval is super fast
@@ -13,12 +15,26 @@ class ProductManager:
         all_products = []
         user_ref = self.db.reference("users")
         users_data = user_ref.get()
-        
+
         if not users_data:
             return []
+
+        user_wise_products: dict = {}
+
+        for user_id , user_info in users_data.items():
+            print(user_info)
+            user_products = user_info.get("products", {})
+            user_wise_products[user_id] = user_products
+
+        return user_wise_products
+
+    def fetch_product_price(self, product: dict) -> None | int:
+        if not product:
+            return None
         
-        for _ , user_info in users_data.item():
-            user_products = user_info.get("products", [])
-            all_products.extend(user_products)
-        
-        return all_products
+        print("product supplied to fetch_product_price", product)
+
+        link = product.get("tracking_link")
+        current_price = float(self.parser.get_price(link).replace(",", ""))
+        print(f"price for product {link} is {current_price}")
+        return current_price
